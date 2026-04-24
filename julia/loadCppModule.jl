4e635d7a038b5a105dbb1761635918feae8df74e
Tatsuyoshi Kurumiya <tckurumiya@berkeley.edu>
2026-04-24 17:44:00 -0600

    Changed reward function and added probabilistic switching back to healthy

--- julia/loadCppModule.jl
+++ julia/loadCppModule.jl
@@ -1,7 +1,19 @@
-# Load custom ompl planning library into CppOMPL module
+# # Load custom ompl planning library into CppOMPL module
+
+# module CppOMPL
+#     using CxxWrap
+#     @wrapmodule(() -> joinpath(@__DIR__, "..", "lib", "JlOMPL.dll"))
+
+#     function __init__()
+#         @initcxx
+#     end
+# end
+
 module CppOMPL
     using CxxWrap
-    @wrapmodule(() -> joinpath(@__DIR__, "../lib", "libJlOMPL"))
+
+    const jlompl_path = normpath(joinpath(@__DIR__, "..", "lib", "JlOMPL.dll"))
+    @wrapmodule(() -> jlompl_path)
 
     function __init__()
         @initcxx
