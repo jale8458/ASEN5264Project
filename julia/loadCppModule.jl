c15df8b9ec4b7c7c05ee9d735d5a8154451edc9a
Tatsuyoshi Kurumiya <tckurumiya@berkeley.edu>
2026-04-24 18:40:43 -0600

    Fixed windows modification overwrite

--- julia/loadCppModule.jl
+++ julia/loadCppModule.jl
@@ -1,19 +1,7 @@
-# # Load custom ompl planning library into CppOMPL module
-
-# module CppOMPL
-#     using CxxWrap
-#     @wrapmodule(() -> joinpath(@__DIR__, "..", "lib", "JlOMPL.dll"))
-
-#     function __init__()
-#         @initcxx
-#     end
-# end
-
+# Load custom ompl planning library into CppOMPL module
 module CppOMPL
     using CxxWrap
-
-    const jlompl_path = normpath(joinpath(@__DIR__, "..", "lib", "JlOMPL.dll"))
-    @wrapmodule(() -> jlompl_path)
+    @wrapmodule(() -> joinpath(@__DIR__, "../lib", "libJlOMPL"))
 
     function __init__()
         @initcxx
